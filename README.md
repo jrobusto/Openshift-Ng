1e9e3249cacd2ff12d854ba442e0ab5f160abea0
Joshua Robusto <jrobusto@42six.com>
2016-03-30 10:17:41 -0700

    Initial commit of scaffolding for webapp

---
+++ README.md
@@ -0,0 +1,14 @@
+# openshift-ng
+
+This project aims to access and serve data from the Openshift API using AngularJS.
+
+This project is generated with [yo angular generator](https://github.com/yeoman/generator-angular)
+version 0.15.1.
+
+## Build & development
+
+Run `grunt` for building and `grunt serve` for preview.
+
+## Testing
+
+Running `grunt test` will run the unit tests with karma.
---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name openshiftNgApp
+ * @description
+ * # openshiftNgApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('openshiftNgApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
